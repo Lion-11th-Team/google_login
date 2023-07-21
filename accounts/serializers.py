05d6d62d41d2bfb224c41868056771dbbc2a257b
DESKTOP-KLIL89F <dain0928@g.hongik.ac.kr>
2023-07-21 15:16:15 +0900

    exclude password

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,13 +4,13 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
-        # exclude = ['password']
+        # fields = '__all__'
+        exclude = ['password']
 
     def create(self, validated_data):
         user = User.objects.create_user(
             email = validated_data['email'],
-            password = validated_data['password'],
+            # password = validated_data['password'],
             oauth_id = validated_data['ouath_id']
         )
         return user
